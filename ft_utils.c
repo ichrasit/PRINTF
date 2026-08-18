7e5c7068a2c49bc95ba01da8d49afd36a3273362
Muhammet Rasit Oz <muhaoz@k1m23s03.42kocaeli.com.tr>
2026-08-19 02:46:59 +0300

    style: resolve all Norminette errors and enforce 42 coding standards

--- ft_utils.c
+++ ft_utils.c
@@ -6,39 +6,45 @@
 /*   By: muhaoz <muhaoz@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/19 02:11:56 by muhaoz            #+#    #+#             */
-/*   Updated: 2026/08/19 02:42:50 by muhaoz           ###   ########.fr       */
+/*   Updated: 2026/08/19 02:46:14 by muhaoz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int	flush_buffer(char *buf, int i)
+{
+	int	len;
+	int	tmp;
+
+	len = 0;
+	while (i > 0)
+	{
+		i--;
+		tmp = write(1, &buf[i], 1);
+		if (tmp == -1)
+			return (-1);
+		len += tmp;
+	}
+	return (len);
+}
+
 int	ft_print_num(unsigned long n, char *base)
 {
 	char			buf[21];
 	int				i;
-	int				len;
-	int				tmp;
 	unsigned long	blen;
 
-	i = 0;
-	len = 0;
 	blen = 0;
-    tmp = 0;
 	while (base[blen])
 		blen++;
 	if (n == 0)
 		return (write(1, &base[0], 1));
+	i = 0;
 	while (n > 0)
 	{
 		buf[i++] = base[n % blen];
 		n /= blen;
 	}
-	while (i > 0)
-	{
-		i--;
-		tmp += write(1, &buf[i], 1);
-		if (tmp == -1)
-			return (-1);
-	}
-	return (len);
+	return (flush_buffer(buf, i));
 }
